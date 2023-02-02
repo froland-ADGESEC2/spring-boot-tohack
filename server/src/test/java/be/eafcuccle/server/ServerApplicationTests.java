db5ee5faa3f1eb349660134ea91c7c881366fca0
François Roland <froland@users.noreply.github.com>
2023-02-02 18:23:39 +0100

    Initial commit

---
+++ server/src/test/java/be/eafcuccle/server/ServerApplicationTests.java
@@ -0,0 +1,12 @@
+package be.eafcuccle.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+
+}
