9010800c5d57acbe3aed2011596428a5871dd8a6
Francois Roland <froland@froland.be>
2023-02-02 17:29:55 +0000

    Restructure project.

---
+++ spring-boot-tohack/src/main/java/be/eafcuccle/froland/sbth/SpringBootTohackApplication.java
@@ -0,0 +1,13 @@
+package be.eafcuccle.froland.sbth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTohackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTohackApplication.class, args);
+	}
+
+}
--- spring-boot-tohack/src/test/java/be/eafcuccle/froland/sbth/SpringBootTohackApplicationTests.java
+++ spring-boot-tohack/src/test/java/be/eafcuccle/froland/sbth/SpringBootTohackApplicationTests.java
@@ -1,12 +1,13 @@
-package be.eafcuccle.server;
+package be.eafcuccle.froland.sbth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServerApplicationTests {
+class SpringBootTohackApplicationTests {
 
-  @Test
-  void contextLoads() {}
+	@Test
+	void contextLoads() {
+	}
 
 }
