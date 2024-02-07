8ee5ff5c98d8c9a1091f9c4acf3cc622b87d5cc0
François Roland <francois.roland@eafc-uccle.be>
2024-02-07 17:01:32 +0000

    Organize imports.

--- src/main/java/be/eafcuccle/froland/sbth/MyController.java
+++ src/main/java/be/eafcuccle/froland/sbth/MyController.java
@@ -1,5 +1,7 @@
 package be.eafcuccle.froland.sbth;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,8 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.Query;
 
 @Controller
 public class MyController {
