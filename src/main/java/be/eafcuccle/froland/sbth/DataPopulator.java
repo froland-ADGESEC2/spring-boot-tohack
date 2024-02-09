6e85bbf88389295a02ba053d591cb7a183232a80
François Roland <francois.roland@eafc-uccle.be>
2024-02-09 11:35:58 +0100

    Add book list. TODO filter the book list.

--- src/main/java/be/eafcuccle/froland/sbth/DataPopulator.java
+++ src/main/java/be/eafcuccle/froland/sbth/DataPopulator.java
@@ -21,8 +21,8 @@ public class DataPopulator implements CommandLineRunner {
 
     Employee admin = employeeRepository.save(new Employee("admin", "admin123"));
     Employee user1 = employeeRepository.save(new Employee("user1", "user1"));
-    employeeRepository.save(new Employee("user2", "user2"));
-    employeeRepository.save(new Employee("user3", "user3"));
+    Employee user2 = employeeRepository.save(new Employee("user2", "user2"));
+    Employee user3 = employeeRepository.save(new Employee("user3", "user3"));
 
     bookRepository.saveAll(List.of(
       new Book("Joshua Bloch", "Effective Java", admin),
@@ -30,7 +30,13 @@ public class DataPopulator implements CommandLineRunner {
       new Book("Kathy Sierra & Bert Bates", "Head First Java", admin),
       new Book("Martin Fowler", "Refactoring", user1),
       new Book("Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides", "Design Patterns", user1),
-      new Book("Brian Goetz", "Java Concurrency in Practice", user1)
+      new Book("Brian Goetz", "Java Concurrency in Practice", user1),
+      new Book("Herbert Schildt", "Java: The Complete Reference", user2),
+      new Book("Craig Walls", "Spring in Action", user2),
+      new Book("Scott Oaks", "Java Performance: The Definitive Guide", user2),
+      new Book("Raoul-Gabriel Urma, Mario Fusco, Alan Mycroft", "Java 8 in Action", user3),
+      new Book("Cay S. Horstmann", "Core Java Volume I–Fundamentals", user3),
+      new Book("Bruce Eckel", "Thinking in Java", user3)
     ));
   }
 
--- src/main/java/be/eafcuccle/froland/sbth/MyController.java
+++ src/main/java/be/eafcuccle/froland/sbth/MyController.java
@@ -3,10 +3,13 @@ package be.eafcuccle.froland.sbth;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
 import java.util.List;
+
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,8 +19,13 @@ public class MyController {
   private EntityManager em;
 
   @GetMapping("/")
-  public String homePage() {
-    return "index";
+  public String homePage(HttpSession session, Model model) {
+    if (session.getAttribute("employee") == null) {
+      return "redirect:/login";
+    } else {
+      model.addAttribute("employee", session.getAttribute("employee"));
+      return "index";
+    }
   }
 
   @GetMapping("/login")
@@ -31,15 +39,29 @@ public class MyController {
   }
 
   @PostMapping("/login")
-  public String checkLogin(@RequestParam String login, @RequestParam String password, Model model) {
+  public String checkLogin(@RequestParam String login, @RequestParam String password, Model model, HttpSession session) {
+    session.removeAttribute("employee");
     Query q = em.createQuery(
         "SELECT e FROM Employee e WHERE e.login='" + login + "' AND e.password='" + password + "'");
     List<Employee> foundEmployees = q.getResultList();
     if (foundEmployees.isEmpty()) {
       return "redirect:/failure";
     } else {
-      model.addAttribute("employee", foundEmployees.get(0));
+      Employee foundEmployee = foundEmployees.get(0);
+      session.setAttribute("employee", foundEmployee);
+      model.addAttribute("employee", foundEmployee);
       return "success";
     }
+  }
+
+  @GetMapping("/books/{employeeId}")
+  public String books(Model model, @PathVariable Integer employeeId) {
+    Query employeeQuery = em.createQuery("SELECT e FROM Employee e WHERE e.id=" + employeeId);
+    Employee employee = (Employee) employeeQuery.getSingleResult();
+    model.addAttribute("employee", employee);
+    Query bookQuery = em.createQuery("SELECT b FROM Book b WHERE b.owner.id=" + employeeId);
+    List<Book> books = bookQuery.getResultList();
+    model.addAttribute("books", books);
+    return "books";
   }
 }
